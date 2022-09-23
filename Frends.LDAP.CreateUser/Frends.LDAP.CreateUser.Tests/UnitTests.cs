2276065f83eeced4ee2a8b909171f8bf8deadd9d
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-09-23 13:56:57 +0300

    init

--- Frends.LDAP.CreateUser/Frends.LDAP.CreateUser.Tests/UnitTests.cs
+++ Frends.LDAP.CreateUser/Frends.LDAP.CreateUser.Tests/UnitTests.cs
@@ -10,7 +10,7 @@ public class UnitTests
     /*
         Create a simple LDAP server to docker
         docker build -t dwimberger/ldap-ad-it 
-        docker run -it --rm -p 10389:10389 dwimberger/ldap-ad-it 
+        docker run -it --rm -p 10389:10389 dwimberger/ldap-ad-it
     */
 
     private readonly string? _host = "127.0.0.1";
