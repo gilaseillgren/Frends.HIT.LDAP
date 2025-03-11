78c89de3d2b07fd918a7d72842cf155168fc63fe
eligil001 <elias.gillgren@hoglandet.se>
2025-03-11 08:35:24 +0100

    Change to simple string temporary

--- Frends.LDAP.SearchObjects/Frends.LDAP.SearchObjects/Definitions/Input.cs
+++ Frends.LDAP.SearchObjects/Frends.LDAP.SearchObjects/Definitions/Input.cs
@@ -1,107 +1,91 @@
-﻿using System;
+﻿﻿using System;
 using System.ComponentModel;
 
-namespace Frends.LDAP.SearchObjects.Definitions
+namespace Frends.LDAP.SearchObjects.Definitions;
+
+/// <summary>
+/// Input parameters.
+/// </summary>
+public class Input
 {
     /// <summary>
-    /// Input parameters.
+    /// The search base parameter specifies the DN of the entry where you want to begin the search.
+    /// If you want the search to begin at the tree root pass an empty string.
     /// </summary>
-    public class Input
-    {
-        /// <summary>
-        /// The search base parameter specifies the DN of the entry where you want to begin the search.
-        /// If you want the search to begin at the tree root pass an empty string.
-        /// </summary>
-        /// <example>"ou=users,dc=wimpi,dc=net"</example>
-        public string SearchBase { get; set; }
-
-        /// <summary>
-        /// The search scope parameter specifies the depth of the search.
-        /// </summary>
-        /// <example>Scopes.ScopeBase</example>
-        public Scopes Scope { get; set; }
-
-        /// <summary>
-        /// The search filter defines the entries that will be returned by the search.
-        /// </summary>
-        /// <example>(title=engineer)</example>
-        public string Filter { get; set; }
-
-        /// <summary>
-        /// The maximum time in milliseconds to wait for results. The default is 0 (no limit).
-        /// </summary>
-        /// <example>0</example>
-        [DefaultValue(0)]
-        public int MsLimit { get; set; }
-
-        /// <summary>
-        /// The maximum time in seconds that the server should spend returning search results. The default is 0 (no limit).
-        /// </summary>
-        /// <example>0</example>
-        [DefaultValue(0)]
-        public int ServerTimeLimit { get; set; }
-
-        /// <summary>
-        /// Specifies when aliases should be dereferenced.
-        /// </summary>
-        /// <example>SearchDereference.DerefNever</example>
-        [DefaultValue(SearchDereference.DerefNever)]
-        public SearchDereference SearchDereference { get; set; }
-
-        /// <summary>
-        /// The maximum number of search results to return for a search request.
-        /// </summary>
-        /// <example>1000</example>
-        [DefaultValue(1000)]
-        public int MaxResults { get; set; }
-
-        /// <summary>
-        /// The number of results to return in a batch.
-        /// </summary>
-        /// <example>1</example>
-        [DefaultValue(1)]
-        public int BatchSize { get; set; }
-
-        /// <summary>
-        /// If true, returns only the attribute names and not their values.
-        /// </summary>
-        /// <example>false</example>
-        [DefaultValue(false)]
-        public bool TypesOnly { get; set; }
-
-        /// <summary>
-        /// List of attributes to retrieve along with an option to choose om deras returtyp.
-        /// Exempel: Key = "objectGUID", ReturnType = AttributeReturnType.Byte
-        /// </summary>
-        public AttributeDefinition[] Attributes { get; set; } = Array.Empty<AttributeDefinition>();
-    }
+    /// <example>"ou=users,dc=wimpi,dc=net"</example>
+    public string SearchBase { get; set; }
 
     /// <summary>
-    /// Definition for an attribute to retrieve.
+    /// The search scope parameter specifies the depth of the search.
     /// </summary>
-    public class AttributeDefinition
-    {
-        /// <summary>
-        /// The name/key of the attribute (e.g. "cn", "objectGUID", etc.).
-        /// </summary>
-        /// <example>cn</example>
-        public string Key { get; set; }
-
-        /// <summary>
-        /// Specifies if value returns as string or byte array om 
-        /// String is default.
-        /// </summary>
-        /// <example>AttributeReturnType.String</example>
-        [DefaultValue(AttributeReturnType.String)]
-        public AttributeReturnType ReturnType { get; set; } = AttributeReturnType.String;
-    }
+    /// <example>Scopes.ScopeBase</example>
+    public Scopes Scope { get; set; }
 
     /// <summary>
-    /// Enum tells wich format attribute will be returned as.
+    /// The search filter defines the entries that will be returned by the search. The LDAP search filter grammar is specified in RFC 2254 and 2251. The grammar uses ABNF notation. If you are looking for all employees with a title of engineer, the search filter would be (title=engineer). 
     /// </summary>
-    public enum AttributeReturnType
-    {
-        String,
-        Byte
-    }
+    /// <example>(title=engineer)</example>
+    public string Filter { get; set; }
+
+    /// <summary>
+    /// The maximum time in milliseconds to wait for results. The default is 0, which means that there is no maximum time limit.
+    /// </summary>
+    /// <example>0</example>
+    [DefaultValue(0)]
+    public int MsLimit { get; set; }
+
+    /// <summary>
+    /// The maximum time in seconds that the server should spend returning search results. This is a server-enforced limit. The default of 0 means no time limit.
+    /// </summary>
+    /// <example>0</example>
+    [DefaultValue(0)]
+    public int ServerTimeLimit { get; set; }
+
+    /// <summary>
+    /// Specifies when aliases should be dereferenced.
+    /// DerefNever, DerefFinding, DerefAlways
+    /// </summary>
+    /// <example>SearchConstraints.DerefNever</example>
+    [DefaultValue(SearchDereference.DerefNever)]
+    public SearchDereference SearchDereference { get; set; }
+
+    /// <summary>
+    /// The maximum number of search results to return for a search request.
+    /// </summary>
+    /// <example>1000</example>
+    [DefaultValue(1000)]
+    public int MaxResults { get; set; }
+
+    /// <summary>
+    /// The number of results to return in a batch. Specifying 0 means to block until all results are received. Specifying 1 means to return results one result at a time.
+    /// </summary>
+    /// <example>1</example>
+    [DefaultValue(1)]
+    public int BatchSize { get; set; }
+
+    /// <summary>
+    /// If true, returns the names but not the values of the attributes found. 
+    /// If false, returns the names and values for attributes found.
+    /// </summary>
+    /// <example>false</example>
+    [DefaultValue(false)]
+    public bool TypesOnly { get; set; }
+
+    /// <summary>
+    /// The names of attributes to retrieve.
+    /// </summary>
+    /// <example>cn</example>
+    public Attributes[] Attributes { get; set; } = Array.Empty<Attributes>();
+}
+
+/// <summary>
+/// Attribute values.
+/// </summary>
+public class Attributes
+{
+    /// <summary>
+    /// Key.
+    /// </summary>
+    /// <example>title</example>
+    public string Key { get; set; }
 }
--- Frends.LDAP.SearchObjects/Frends.LDAP.SearchObjects/SearchObjects.cs
+++ Frends.LDAP.SearchObjects/Frends.LDAP.SearchObjects/SearchObjects.cs
@@ -1,4 +1,4 @@
-﻿using Frends.LDAP.SearchObjects.Definitions;
+﻿﻿using Frends.LDAP.SearchObjects.Definitions;
 using System.ComponentModel;
 using Novell.Directory.Ldap;
 using System;
@@ -75,31 +75,9 @@ public class LDAP
                     {
                         LdapAttribute attribute = ienum.Current;
                         var attributeName = attribute.Name;
-                        string attributeVal;
-
-                        var def = input.Attributes.Find(a => a.Key.Equals(attributeName, StringComparison.OrdinalIgnoreCase));
-
-                        if (def != null && def.ReturnType.Equals("Byte", StringComparison.OrdinalIgnoreCase))
-                        {
-                            byte[] bytes = attribute.ByteValue;
-                            if (attributeName.Equals("objectGUID", StringComparison.OrdinalIgnoreCase))
-                            {
-                                Guid guid = new Guid(bytes);
-                                attributeVal = guid.ToString();
-                            }
-                            else
-                            {
-                                attributeVal = BitConverter.ToString(bytes).Replace("-", "");
-                            }
-                        }
-                        else
-                        {
-                            attributeVal = attribute.StringValue;
-                        }
-
+                        var attributeVal = attribute.StringValue;
                         attributeList.Add(new AttributeSet { Key = attributeName, Value = attributeVal });
                     }
-
 
                     searchResults.Add(new SearchResult() { DistinguishedName = entry.Dn, AttributeSet = attributeList });
                 }
